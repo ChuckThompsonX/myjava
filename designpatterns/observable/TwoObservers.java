e89a5ebdbdbb91b21cac8256c9c1421b35f1d694
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-14 09:33:44 -0800

    Create TwoObservers.java

---
+++ designpatterns/observable/TwoObservers.java
@@ -0,0 +1,17 @@
+package myjava.designpatterns.observable;
+
+public class TwoObservers {
+    
+    public static void main(String... args) {
+        BeingWatched observed = new BeingWatched();
+        Watcher1 observing1 = new Watcher1();
+        Watcher2 observing2 = new Watcher2();
+        
+        // Register both observers to BeingWatched
+        observed.addObserver(observing1);
+        observed.addObserver(observing2);
+        
+        observed.counter(8);
+    }
+    
+}
