81504838719691be5aa111f04b56fc82cbb7b755
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-16 16:10:00 -0800

    Create FactoryPatternDemo.java

---
+++ designpatterns/factory/FactoryPatternDemo.java
@@ -0,0 +1,16 @@
+package myjava.designpatterns.factory;
+
+// client code
+public class FactoryPatternDemo {
+	
+    public static void main(String... args) {
+    	
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        IShape shape = shapeFactory.getShape(Shape.CIRCLE);
+        shape.draw();
+
+        shape = shapeFactory.getShape(Shape.SQUARE);
+        shape.draw();
+    }
+}
