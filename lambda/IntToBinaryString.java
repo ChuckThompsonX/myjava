d6c83c8ccf4c07f4c450a512b528084b711e34b9
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-21 19:01:49 -0800

    Create IntToBinaryString.java

---
+++ lambda/IntToBinaryString.java
@@ -0,0 +1,16 @@
+package myjava.lambda;
+
+import java.util.function.Function;
+
+public class IntToBinaryString {
+
+	public static void main(String[] args) {
+		
+		// generic functional interface provided by Java
+		// 1st param is integer value passed into lambda function
+		// 2nd param is the string result
+		Function<Integer, String> func = num -> Integer.toBinaryString(num);
+		String str = func.apply(10); // run the apply method on the functional interface
+		System.out.println(str);
+	}
+}
