c7ecb8afa975bf8ff918f63aff483b35635165a5
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-23 17:02:10 -0800

    Create SumOddNumbers.java

---
+++ streams/SumOddNumbers.java
@@ -0,0 +1,20 @@
+package myjava.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumOddNumbers {
+
+	public static void main(String... args) {
+		
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+		
+		// sum of all odd numbers
+		int sum = numbers.stream()
+				.filter(x -> x % 2 != 0) // returns plain old stream
+				.mapToInt(Integer::intValue) // you need an Int stream
+				.sum(); // in order to do this sum
+		
+		System.out.println(sum);
+	}
+}
