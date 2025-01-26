cd8caac2f37913cb1917a2c036d686eb853df547
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-25 16:49:32 -0800

    Create StaticDemo.java

---
+++ test/StaticDemo.java
@@ -0,0 +1,53 @@
+package myjava.test;
+
+public class StaticDemo {
+
+	public static final String name = "NAME";
+	
+	{
+		System.out.println("HELLO");
+	}
+	
+	static {
+		show();
+	}
+	
+	public StaticDemo() {
+		System.out.println("CONSTRUCTOR");
+	}
+	
+	public static void show() {
+		System.out.println("MESSAGE");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("MAIN");
+		
+		new StaticDemo();
+		new StaticDemo();
+	}
+	
+	static {
+		System.out.println("STATIC BLOCK");
+		System.out.println("StaticDemo.name = " + name);
+	}
+}
+
+/*
+static
+- good for loading drivers
+- initializing static members
+- security
+- logging
+- some DB operations e.g. prepared SQL statement
+
+MESSAGE
+STATIC BLOCK
+StaticDemo.name = NAME
+MAIN
+HELLO
+CONSTRUCTOR
+HELLO
+CONSTRUCTOR
+ */
