43d2f355d29b8d29fa49ac3c8536c182e1a756e0
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-25 16:51:28 -0800

    Create CompDemo.java

---
+++ comparator/CompDemo.java
@@ -0,0 +1,24 @@
+package myjava.comparator;
+
+import java.util.TreeSet;
+
+public class CompDemo {
+    
+    public static void main(String... args) {
+        // create a tree set
+        TreeSet<String> ts = new TreeSet<>(new MyComp());
+        
+        // Add elements to the tree set
+        ts.add("C");
+        ts.add("A");
+        ts.add("B");
+        ts.add("B");
+        ts.add("E");
+        ts.add("F");
+        ts.add("D");
+        
+        // display the elements
+        treeSet.stream()
+            .forEach(System.out::println);
+    }
+}
