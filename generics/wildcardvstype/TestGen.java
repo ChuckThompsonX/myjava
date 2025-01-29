39b3536d93a3f1739a9d8c7b87b1946bb7af6312
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-28 21:21:59 -0800

    Update TestGen.java

--- generics/wildcardvstype/TestGen.java
+++ generics/wildcardvstype/TestGen.java
@@ -8,32 +8,32 @@ public class TestGen {
 
 	public static void main(String[] args) {
 
-    // this example should clarify the differences between wild card ? and type T
-    
+		// this example should clarify the differences between wild card ? and type T
+
 		List<Animal> animalList = List.of(new Animal("Cat", "Basil"));
 		Collection<Animal> animals = createNewList(animalList);
 		animals.stream().forEach((a) -> System.out.println(a.getType()));
 
-    List<Integer> ints = List.of(3);
-    List<Double> doubles = List.of(3.14);
-    List<String> strings = List.of("hi");
+		List<Integer> ints = List.of(3);
+		List<Double> doubles = List.of(3.14);
+		List<String> strings = List.of("hi");
 
-    printAnyList(ints);
-    printAnyList(doubles);
-    printAnyList(strings);
-    printTheList(ints);
-    printTheList(doubles);
-    printTheList(strings);
+		printAnyList(ints);
+		printAnyList(doubles);
+		printAnyList(strings);
+		printTheList(ints);
+		printTheList(doubles);
+		printTheList(strings);
 
-    printNumbers(ints);
-    printNumbers(doubles);
+		printNumbers(ints);
+		printNumbers(doubles);
 	}
 
 	private static <T extends Animal> Collection<T> createNewList(List<T> toAdd) {
-	    List<T> list = new LinkedList<>();
-	    list.add(toAdd.getFirst());
-	    return list;
-	}	
+		List<T> list = new LinkedList<>();
+		list.add(toAdd.getFirst());
+		return list;
+	}
 
 	// use ? when you don't know what that generic type is going to be
 	private static void printAnyList(List<?> list) {
@@ -41,15 +41,16 @@ public class TestGen {
 		System.out.println(obj.getClass().getTypeName());
 	}
 
-	// use type T when you are actively working with the type or when you have a return of the type
+	// use type T when you are actively working with the type or when you have a
+	// return of the type
 	private static <T> void printTheList(List<T> list) {
 		T t = list.getFirst();
 		System.out.println(t.getClass().getTypeName());
 	}
 
 	private static void printNumbers(List<? extends Number> list) {
-        for (Number num : list) {
-            System.out.println(num.doubleValue());
-        }
-    }
+		for (Number num : list) {
+			System.out.println(num.doubleValue());
+		}
+	}
 }
