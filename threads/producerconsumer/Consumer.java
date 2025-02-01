730b66855e11928edcc6a6de0eaae47ad9d04156
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-01 09:58:02 -0800

    Create Consumer.java

---
+++ threads/producerconsumer/Consumer.java
@@ -0,0 +1,26 @@
+package myjava.threads.producerconsumer;
+
+public class Consumer implements Runnable {
+    
+    QStructure q;
+    Thread t;
+    
+    Consumer(QStructure q) {
+        this.q = q;
+        this.t = new Thread(this, "Consumer");
+        this.t.start();
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            q.get();
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                // Do nothing
+            }
+        }
+    }
+}
