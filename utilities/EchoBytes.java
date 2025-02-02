be08fe433d05eb0aaf9ec6a6fe7252e816436517
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-02 10:11:05 -0800

    Create EchoBytes.java

---
+++ utilities/EchoBytes.java
@@ -0,0 +1,26 @@
+package myjava.utilities;
+
+import java.io.InputStream;
+import java.io.IOException;
+
+public class EchoBytes {
+
+  	public static void main(String... args) {
+		try {
+			byte[] bytes = new byte[10];
+
+			// Read from console input
+			InputStream stream = System.in;
+			int bytesRead = stream.read(bytes);
+			System.out.println(bytesRead);
+			
+			for (int idx = 0; idx < bytes.length; idx++) {
+				char ch = (char) bytes[idx];
+				System.out.print(ch);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
