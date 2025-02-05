b79fa993e12648feca591a0b69e95516d1a5cc24
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-05 07:35:56 -0800

    Create TestInner.java

---
+++ inner/TestInner.java
@@ -0,0 +1,10 @@
+package com.java.inner;
+
+public class TestInner {
+
+	public static void main(String[] args) {
+		OuterClass st = new OuterClass();
+		OuterClass.InnerClass fl = st.new InnerClass();
+        fl.methodInFirstLevel(23);
+	}
+}
