82b501fc5acfcc97c57b87d20e25023c16aee972
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-08 20:02:35 -0800

    Create Rectangle.java

---
+++ poly/Rectangle.java
@@ -0,0 +1,14 @@
+package myjava.poly;
+
+public class Rectangle extends Figure {
+
+    Rectangle(double a, double b) {
+        super(a, b);
+    }
+
+    @Override
+    double area() {
+        System.out.println("Inside Area for Rectangle.");
+        return dim1 * dim2;
+    }
+}
