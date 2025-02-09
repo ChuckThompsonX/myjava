9c98b3f6191b0c2fd96de24b4ffb7aa5990fead1
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-08 20:03:00 -0800

    Create Triangle.java

---
+++ poly/Triangle.java
@@ -0,0 +1,14 @@
+package myjava.poly;
+
+public class Triangle extends Figure {
+
+    Triangle(double a, double b) {
+        super(a, b);
+    }
+
+    @Override
+    double area() {
+        System.out.println("Inside Area for Triangle.");
+        return dim1 * dim2 / 2;
+    }
+}
