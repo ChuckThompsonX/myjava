75df41ad824733be97e17716134f3b0fdf0502a4
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-09 13:44:45 -0800

    Create BoxWeight.java

---
+++ inheritance/BoxWeight.java
@@ -0,0 +1,20 @@
+package myjava.inheritance;
+
+public class BoxWeight extends Box {
+
+    double weight;
+    
+    BoxWeight(double m) {
+        super(0.0, 0.0, 0.0);
+        weight = m;
+    }
+    
+    BoxWeight(double w, double h, double d, double m) {
+        //width = w;
+        //height = h;
+        //depth = d;
+        super(w, h, d);
+        weight = m;
+    }
+    
+}
