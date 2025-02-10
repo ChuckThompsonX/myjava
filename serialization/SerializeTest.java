c4409ba5e1b1276586dd08446cd87291f4165501
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-10 06:57:34 -0800

    Create SerializeTest.java

---
+++ serialization/SerializeTest.java
@@ -0,0 +1,34 @@
+package myjava.serialize;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SerializeTest {
+
+	public static void main(String... args) {
+		// Set storage
+		String root = File.listRoots()[0].getAbsolutePath();
+		String fileName = root + File.separator + "temp" + File.separator;
+		fileName += "employee.ser";
+		
+		// Give object state
+		Employee emp = new Employee();
+		emp.setId(101);
+		emp.setName("Chuck");
+		emp.setSalary(50000);
+
+		try {
+			// Serialize to file
+			FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(emp);
+			fos.close();
+
+			System.out.println("emp Obj->" + emp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
