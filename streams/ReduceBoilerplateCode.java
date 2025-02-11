69923b3f457bae57d958971624913b0f22244559
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-11 07:18:25 -0800

    Create ReduceBoilerplateCode.java

---
+++ streams/ReduceBoilerplateCode.java
@@ -0,0 +1,28 @@
+package myjava.streams;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ReduceBoilerplateCode {
+
+	public static void main(String... args) {
+		
+        List<String> names = Arrays.asList("Doug", "Susan", "Larry");
+
+        Collections.sort(names, new Comparator<>() {
+            @Override
+            public int compare(String n1, String n2) {
+                return n1.compareTo(n2);
+            }
+        });
+
+        System.out.println(names);
+        
+        // replaces the anonymous inner class above reducing lines of code
+        names.sort((n1, n2) -> n1.compareTo(n2));
+
+        System.out.println(names);
+	}
+}
