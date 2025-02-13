2c2376ff2fb466159e1ac064ac1b76da41659618
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-13 10:20:40 -0800

    Create Email.java

---
+++ di/Email.java
@@ -0,0 +1,30 @@
+package myjava.di;
+
+public class Email {
+
+	private String message;
+	
+	public Email() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void sendMessage(String message) {
+		this.setMessage(message);
+		// SMTP send message
+		System.out.println("Sent message " + message);
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
