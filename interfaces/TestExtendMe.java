fa7ff7dc7e24685c0e16026ae9577e57eed62907
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-14 09:55:49 -0800

    Create TestExtendMe.java

---
+++ interfaces/TestExtendMe.java
@@ -0,0 +1,17 @@
+package myjava.interfaces;
+
+public class TestExtendMe implements IExtendTestMe {
+
+	public void printMe() {
+		System.out.println("Test");
+	}
+	
+	public int computeMe() {
+		return 2 * 5;
+	}
+	
+	public int computeMeToo(int x) {
+		return x * 2;
+	}
+
+}
