046b8843eed7d66632c2d334a6023fb16ded501e
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-15 08:45:54 -0800

    Create SimpleDBConnect.java

---
+++ database/mysql/SimpleDBConnect.java
@@ -0,0 +1,29 @@
+package myjava.database;
+
+import java.sql.*;
+
+public class SimpleDBConnect {
+
+	public static void main(String... args) {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "pwdpwd");
+			Statement stmt = conn.createStatement();
+			String sql = "select name from test";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				String name = rs.getString("name");
+				System.out.println(name);
+			}
+
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
