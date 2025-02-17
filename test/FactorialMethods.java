74efa6693fbdede4e249629f77e4c414e095f186
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-17 14:00:16 -0800

    Create FactorialMethods.java

---
+++ test/FactorialMethods.java
@@ -0,0 +1,30 @@
+package myjava.test;
+
+public class FactorialMethods {
+
+	static int fact(int n) {
+		if (n == 1) return 1;
+		return fact(n - 1) * n;
+	}
+	
+	interface Fact {
+		int func(int n);
+	}
+	
+	public static void main(String[] args) {
+		
+		Fact f = (n) -> {
+			int result = 1;
+			for (int i = 1; i <= n; i++) {
+				result = i * result;
+			}
+			return result;
+		};
+
+		int factorial = f.func(5);
+		System.out.println(factorial);
+		
+		factorial = fact(5);
+		System.out.println(factorial);
+	}
+}
