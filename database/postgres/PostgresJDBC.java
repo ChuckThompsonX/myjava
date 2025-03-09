8cd29eeb3e8f790e97f09858bdbfb3a52e2f1493
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-08 19:54:22 -0800

    Update PostgresJDBC.java

--- database/postgres/PostgresJDBC.java
+++ database/postgres/PostgresJDBC.java
@@ -10,12 +10,10 @@ public class PostgresJDBC {
 
 	public static void main(String[] args) {
 
-		System.out.println("-------- PostgreSQL JDBC Connection Testing ------------");
-
 		try {
 			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+		} 
+		catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			return;
 		}
@@ -24,21 +22,21 @@ public class PostgresJDBC {
 		Connection connection = null;
 
 		try {
-			connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "postgres");
+			connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "xxxxxxxxx");
 			Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery("select * from names");
-
-            if (rs.next()) {
-                System.out.println(rs.getString(2));
-            }
-		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
+            		ResultSet rs = st.executeQuery("select * from names");
+			
+			if (rs.next()) {
+				System.out.println(rs.getString(2));
+			}
+		} 
+		catch (SQLException e) {
 			e.printStackTrace();
 			return;
 		}
 
 		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
+			System.out.println("Connection successful!");
 		}
 	}
 }
