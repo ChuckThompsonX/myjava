f40806a216320a6320f95d3b8e9ab28caeb2c9de
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-10 14:47:41 -0700

    Update Consumer.java

--- threads/producerconsumer/Consumer.java
+++ threads/producerconsumer/Consumer.java
@@ -18,7 +18,8 @@ public class Consumer implements Runnable {
 
             try {
                 Thread.sleep(500);
-            } catch (InterruptedException ex) {
+            } 
+            catch (InterruptedException ex) {
                 // Do nothing
             }
         }
