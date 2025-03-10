66b5f30ddaeee6b2c1c4e8bd2cf796d63d452d9c
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-10 14:50:16 -0700

    Update ThreadA.java

--- threads/deadlock/ThreadA.java
+++ threads/deadlock/ThreadA.java
@@ -6,25 +6,17 @@ public class ThreadA extends Thread {
 	
 	private Object obj2;
 	
-	/**
-	 * 
-	 * @param obj1
-	 * @param obj2
-	 */
 	ThreadA(Object obj1, Object obj2) {
 		this.obj1 = obj1;
 		this.obj2 = obj2;
 	}
 	
-	/**
-	 * 
-	 */
 	public void run() {
 		synchronized (obj1) {
 			System.out.println("ThreadA: Holding lock 1...");
 			System.out.println("ThreadA: Waiting for lock 2...");
 			
-			// Already inside 1 now we want 2
+			// already inside 1 now we want 2
 			synchronized (obj2) {
 				System.out.println("ThreadA: Holding lock 1 & 2...");
 			}
