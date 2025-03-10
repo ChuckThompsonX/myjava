60719b15dfade8719fc8f6af8decfdc4f3b8577c
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-10 14:51:28 -0700

    Update FactorialMethods.java

--- test/FactorialMethods.java
+++ test/FactorialMethods.java
@@ -11,7 +11,7 @@ public class FactorialMethods {
 		int func(int n);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		
 		Fact f = (n) -> {
 			int result = 1;
