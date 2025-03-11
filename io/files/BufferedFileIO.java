f358af9443b7a7e1f7c0da0be1c59d25ab4fe04d
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-11 10:46:10 -0700

    Update BufferedFileIO.java

--- io/files/BufferedFileIO.java
+++ io/files/BufferedFileIO.java
@@ -26,22 +26,24 @@ public class BufferedFileIO {
 		Path file1 = Paths.get(path + "bufferedR.txt");
 		Path file2 = Paths.get(path + "bufferedW.txt");
 		
-		// Read from file
+		// read from file
 		try (BufferedReader reader = Files.newBufferedReader(file1, charset)) {
-		    String line = null;
-		    while ((line = reader.readLine()) != null) {
-		        System.out.println(line);
-		    }
-		} catch (IOException ioe) {
-		    ioe.printStackTrace();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+		        	System.out.println(line);
+			}
+		} 
+		catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
 		
-		// Write to file
+		// write to file
 		String s = "Write this message to file 123.";
 		try (BufferedWriter writer = Files.newBufferedWriter(file2, charset)) {
-		    writer.write(s, 0, s.length());
-		} catch (IOException ioe) {
-		    ioe.printStackTrace();
+		    	writer.write(s, 0, s.length());
+		} 
+		catch (IOException ioe) {
+		    	ioe.printStackTrace();
 		}
 	}
 }
