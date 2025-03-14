aa45934b1278f378f1a019beaf9b02d58782e074
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-13 18:20:36 -0700

    Update Defensive.java

--- designpatterns/strategy/Defensive.java
+++ designpatterns/strategy/Defensive.java
@@ -1,17 +1,7 @@
-/**
- * Defensive
- */
 package com.java.strategy;
 
-/**
- * @author Chuck
- *
- */
 public class Defensive implements IBehavior {
 
-	/**
-	 * 
-	 */
 	public void move() {
 		System.out.println("I'm protecting myself.");
 	}
