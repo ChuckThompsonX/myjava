407e6a5d6801e4abc6e84b7dca44340c367e5800
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-13 18:21:24 -0700

    Update Submissive.java

--- designpatterns/strategy/Submissive.java
+++ designpatterns/strategy/Submissive.java
@@ -1,19 +1,8 @@
-/**
- * Submissive
- */
 package com.java.strategy;
 
-/**
- * @author Chuck
- *
- */
 public class Submissive implements IBehavior {
 
-	/**
-	 * 
-	 */
 	public void move() {
 		System.out.println("Just going with the flow.");
 	}
-
 }
