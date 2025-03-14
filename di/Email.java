f64c6f9d86b4e9dfee6b53fbbc1e32ff90e77b93
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-13 18:21:58 -0700

    Update Email.java

--- di/Email.java
+++ di/Email.java
@@ -5,7 +5,6 @@ public class Email {
 	private String message;
 	
 	public Email() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public void sendMessage(String message) {
@@ -14,16 +13,10 @@ public class Email {
 		System.out.println("Sent message " + message);
 	}
 
-	/**
-	 * @return the message
-	 */
 	public String getMessage() {
 		return message;
 	}
 
-	/**
-	 * @param message the message to set
-	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
