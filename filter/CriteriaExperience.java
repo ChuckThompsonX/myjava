f285689546cfdeeeaafcfa77ac4da33cc259bff4
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-14 12:27:24 -0700

    Update CriteriaExperience.java

--- filter/CriteriaExperience.java
+++ filter/CriteriaExperience.java
@@ -7,13 +7,15 @@ public class CriteriaExperience implements Criteria {
 
    @Override
    public List<Developer> meetCriteria(List<Developer> developers) {
+      
       List<Developer> singlePersons = new ArrayList<Developer>(); 
       
       for (Developer developer : developers) {
-         if(developer.getExperience().equalsIgnoreCase("10")){
+         if (developer.getExperience().equalsIgnoreCase("10")){
             singlePersons.add(developer);
          }
       }
+      
       return singlePersons;
    }
 }
