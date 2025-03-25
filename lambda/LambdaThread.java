b2603a22870c5d780e880f0bc8bcde5f40f4bea5
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-24 17:41:15 -0700

    Update LambdaThread.java

--- lambda/LambdaThread.java
+++ lambda/LambdaThread.java
@@ -4,15 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 public class LambdaThread {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		
 		System.out.println("Test Lambda Thread Start");
 		
 		Runnable r = () -> {
 			for (int i = 0; i < 10; i++) {
-				
 				System.out.println(i);
-				
 				try {
 					TimeUnit.SECONDS.sleep(1);
 				} 
