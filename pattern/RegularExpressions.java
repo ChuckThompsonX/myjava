ca6b2b8481181551b946bd505c87dc676d29aea8
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-24 17:44:15 -0700

    Update RegularExpressions.java

--- pattern/RegularExpressions.java
+++ pattern/RegularExpressions.java
@@ -5,11 +5,9 @@ import java.util.regex.Matcher;
 
 public class RegularExpressions {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String... args) {
-		// Case sensitive
+	public static void main(String[] args) {
+		
+		// case sensitive
 		Pattern pat = Pattern.compile("Data");
 		Matcher mat = pat.matcher("data");
 		boolean found = mat.matches();
@@ -19,7 +17,7 @@ public class RegularExpressions {
 		found = mat.matches();
 		System.out.println("Testing Data vs. Data 10: " + found);
 		
-		// Subsequences
+		// subsequences
 		pat = Pattern.compile("data");
 		mat = pat.matcher("data 5 4 3 2 1 data");
 		
@@ -27,7 +25,7 @@ public class RegularExpressions {
 			System.out.println("data found at index " + mat.start());
 		}
 		
-		// Using wildcards and quantifiers
+		// using wildcards and quantifiers
 		pat = Pattern.compile("H+");
 		mat = pat.matcher("H HH HHH");
 		
@@ -49,7 +47,7 @@ public class RegularExpressions {
 			System.out.println("Match3: " + mat.group());
 		}
 		
-		// Classes of characters
+		// classes of characters
 		pat = Pattern.compile("[a-z]+");
 		mat = pat.matcher("read more play less.");
 		
@@ -57,7 +55,7 @@ public class RegularExpressions {
 			System.out.println("Match4: " + mat.group());
 		}
 		
-		// Using replace all
+		// using replace all
 		String str = "Jeb Jebra Bob Mac Grant";
 		pat = Pattern.compile("Jeb.*? ");
 		mat = pat.matcher(str);
@@ -66,7 +64,7 @@ public class RegularExpressions {
 		str = mat.replaceAll("Jeb ");
 		System.out.println("Modified sequence: " + str);
 		
-		// Using split
+		// using split
 		pat = Pattern.compile("[ ,.-]");
 		String[] strs = pat.split("alpha omega,beta5 10-over.big");
 		
