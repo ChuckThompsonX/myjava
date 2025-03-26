930f8459f9902bd22edbfded46d52368aed5371b
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-25 20:10:58 -0700

    Update FindThirdLargestInteger.java

--- streams/FindThirdLargestInteger.java
+++ streams/FindThirdLargestInteger.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class FindThirdLargestInteger {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		
 		// find the third largest integer
 		
