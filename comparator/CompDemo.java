592dfce675cde8b0a0f836b8184c7559c6249b1d
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-03 07:58:41 -0700

    Update CompDemo.java

--- comparator/CompDemo.java
+++ comparator/CompDemo.java
@@ -4,7 +4,7 @@ import java.util.TreeSet;
 
 public class CompDemo {
     
-    public static void main(String... args) {
+    public static void main(String[] args) {
         // create a tree set
         TreeSet<String> ts = new TreeSet<>(new MyComp());
         
