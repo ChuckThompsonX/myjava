643b3542822c20a24bff7dc9349a2f63fdf69a4c
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-08 06:53:26 -0700

    Update TestInner.java

--- inner/TestInner.java
+++ inner/TestInner.java
@@ -2,7 +2,7 @@ package com.java.inner;
 
 public class TestInner {
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		OuterClass st = new OuterClass();
 		OuterClass.InnerClass fl = st.new InnerClass();
         	fl.methodInFirstLevel(23);
