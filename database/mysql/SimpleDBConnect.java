768f8acc0fb9fcf593d24df03169f1676dc9da04
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-13 08:29:34 -0700

    Update SimpleDBConnect.java

--- database/mysql/SimpleDBConnect.java
+++ database/mysql/SimpleDBConnect.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class SimpleDBConnect {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
