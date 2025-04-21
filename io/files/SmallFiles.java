9c8d83e140211e303d23fcfa4ebeecc80a9fd245
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-21 11:36:15 -0700

    Update SmallFiles.java

--- io/files/SmallFiles.java
+++ io/files/SmallFiles.java
@@ -9,7 +9,7 @@ import java.nio.file.StandardOpenOption;
 
 public class SmallFiles {
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		
 		String root = File.listRoots()[0].getAbsolutePath();
 		String path = root + File.separator + "temp" + File.separator;
