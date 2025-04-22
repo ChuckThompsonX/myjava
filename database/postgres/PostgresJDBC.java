dd1cce2aa9efa6c20be16bbaea68273ca7c18087
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-04-22 07:07:46 -0700

    Update PostgresJDBC.java

--- database/postgres/PostgresJDBC.java
+++ database/postgres/PostgresJDBC.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class PostgresJDBC {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 
 		try {
 			Class.forName("org.postgresql.Driver");
