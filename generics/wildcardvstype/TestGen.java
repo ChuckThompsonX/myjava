0d795fcffbf5e12b4a5df9f5a5e98b8a3e5e9fb2
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-05-04 14:10:10 -0700

    Update TestGen.java

--- generics/wildcardvstype/TestGen.java
+++ generics/wildcardvstype/TestGen.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class TestGen {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 
 		// this example should clarify the differences between wild card ? and type T
 
