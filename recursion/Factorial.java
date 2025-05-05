b5c18391fd15a1409489ac3ddb8575efe767bef2
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-05-05 08:24:10 -0700

    Update Factorial.java

--- recursion/Factorial.java
+++ recursion/Factorial.java
@@ -2,7 +2,7 @@ package myjava.recursion;
 
 public class Factorial {
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		int f = fact(5);
 		System.out.println(f);
 	}
